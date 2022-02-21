c2c4c2cf0772f01891e7cdd9f78ae7785fbfd0e7
lcyper <lcyper@gmail.com>
2022-02-21 12:37:43 -0300

    add isColorInverted property

--- lib/invert_colors.dart
+++ lib/invert_colors.dart
@@ -4,11 +4,16 @@ import 'package:flutter/widgets.dart';
 
 class InvertColors extends StatelessWidget {
   final Widget child;
+  /// decide invert or not
+  final bool isColorInverted;
 
-  const InvertColors({required this.child});
+  const InvertColors({required this.child, this.isColorInverted = true});
 
   @override
   Widget build(BuildContext context) {
+    if (isColorInverted == false) {
+      return child;
+    }
     return ColorFiltered(
       colorFilter: ColorFilter.matrix([
         -1, //RED
